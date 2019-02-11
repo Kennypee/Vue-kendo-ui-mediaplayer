fcbdac364053d658a2b18a8d12e952e857dc859d
kennypee <ekeneeze3@gmail.com>
2019-02-11 17:36:56 +0100

    initial commit

---
+++ src/components/Mediaplayer.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <h1>Kendo ui Mediaplayer demo</h1>
+    <div id="vueapp" class="vue-app">
+      <div class="col-xs-12 col-sm-6 example-col">
+        <div class="h-100 row align-items-center">
+        <kendo-mediaplayer
+          style="height: 500px; margin: 150px"
+          :auto-play="true"
+          :navigatable="true"
+          :media-title="'Kendo ui Media player demo in Vue.js'"
+          :media-source="'https://www.youtube.com/watch?v=tc3xhD24iTU'"
+        ></kendo-mediaplayer>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "@progress/kendo-ui";
+import "@progress/kendo-theme-default/dist/all.css";
+export default {
+  name: "Mediaplayer",
+
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '@progress/kendo-ui'
+import '@progress/kendo-theme-default/dist/all.css'
+
+import { MediaPlayer, MediaPlayerInstaller } from '@progress/kendo-mediaplayer-vue-wrapper'
+Vue.use(MediaPlayerInstaller)
 
 Vue.config.productionTip = false
 
 new Vue({
+  components: {
+    MediaPlayer
+  },
   render: h => h(App),
 }).$mount('#app')
